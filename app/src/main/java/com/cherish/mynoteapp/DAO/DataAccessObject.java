8d5fafe43f9aef813138decb0d08aea737b66935
Gabriel Hephzibah <gabrielhephzibah24@gmail.com>
2020-02-11 19:27:17 +0100

    Added entity folder and data access object

---
+++ app/src/main/java/com/cherish/mynoteapp/DAO/DataAccessObject.java
@@ -0,0 +1,27 @@
+package com.cherish.mynoteapp.DAO;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.cherish.mynoteapp.entity.NoteItem;
+
+import java.util.List;
+
+
+@Dao
+public interface DataAccessObject {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(NoteItem noteItem);
+
+    @Query("DELETE FROM note_table")
+    void  delete();
+
+    @Query("SELECT * from note_table ORDER BY content  ASC")
+    List<NoteItem> getItemAlphabetically();
+
+
+
+}
---
+++ app/src/main/java/com/cherish/mynoteapp/entity/NoteItem.java
@@ -0,0 +1,43 @@
+package com.cherish.mynoteapp.entity;
+
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
+public class NoteItem {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "heading")
+    private  String heading;
+
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "content")
+    private  String content;
+
+    public NoteItem(String heading, String content) {
+        this.heading = heading;
+        this.content = content;
+    }
+
+    public String getHeading() {
+        return heading;
+    }
+
+    public void setHeading(String heading) {
+        this.heading = heading;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
